0ea26d6eafffa6f29656441224469a8a4e948f6d
Julian Bonnells <17135680+jbonnells@users.noreply.github.com>
2025-02-16 11:32:05 -0700

    readme and docker scripts

--- join_docker.sh
+++ join_docker.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-docker ps
-read -rp "Enter the container ID you want to connect to: " container_id
+if [ -n "$1" ]; then
+  container_id="$1"
+else
+  docker ps
+  read -rp "Enter the container ID you want to connect to: " container_id
+fi
+
 docker exec -it "$container_id" bash
